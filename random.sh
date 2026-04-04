6cb10b72c69b63da67ec0ce450393518cd918820
niu541412 <niu541412@gmail.com>
2026-04-05 00:37:51 +0800

    update random.sh

--- random.sh
+++ random.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+# picture rotation, random order, and change interval are not supported in MacOS 26(maybe, so disabled)
 /usr/bin/osascript <<END
 tell application "System Events"
 	tell every desktop
-		set picture rotation to 1 -- 0=off | 1=interval | 2=login | 3=sleep
-		set random order to true
+		-- set picture rotation to 1 -- 0=off | 1=interval | 2=login | 3=sleep
+		-- set random order to true
 		set pictures folder to "/Library/Desktop Pictures"
-		set change interval to 3600 -- seconds
+		-- set change interval to 3600 -- seconds
 	end tell
 end tell
 END
